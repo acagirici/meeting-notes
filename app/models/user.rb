273ab329f207a8d610cc802fac49c680fe42f41d
acagirici <acagirici23@gmail.com>
2021-02-06 23:41:06 -0500

    done-two

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :name, presence: { message: "must be given please" }
-    validates :last_name, presence: { message: "must be given please" }
-    validates :email, presence: { message: "must be given please" }
-    validates :email, uniqueness: { message: "email address already taken" }
+    validates :name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
     
     has_many :meetings
 
